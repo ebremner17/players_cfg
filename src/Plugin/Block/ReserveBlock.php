23b92ac8045a441a555c319ee2bdd112c4bf72ee
ebremner <ebremner@uwaterloo.ca>
2022-12-09 09:29:16 -0500

    Adding stuff for reserve block

---
+++ src/Plugin/Block/ReserveBlock.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Drupal\players_cfg\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\players_reserve\Service\PlayersService;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Copy text block.
+ *
+ * @Block(
+ *   id = "cbl_reserve",
+ *   admin_label = @Translation("Reserve"),
+ * )
+ */
+class ReserveBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The players service.
+   *
+   * @var \Drupal\players_reserve\Service\PlayersService
+   */
+  protected $playersService;
+
+  /**
+   * Constructs a BlockComponentRenderArray object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\players_reserve\Service\PlayersService $playersService
+   *   The players service.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    PlayersService $playersService
+  ) {
+
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->playersService = $playersService;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(
+    ContainerInterface $container,
+    array $configuration,
+    $plugin_id,
+    $plugin_definition
+  ) {
+
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('players_reserve.players_service')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+
+    // Get the correct date.
+    $date = $this->playersService->getCorrectDate();
+
+    // Get the node based on the current date.
+    $node = $this->playersService->getGameNodeByDate($date);
+
+    // If there is node, get the info about the games.
+    if ($node) {
+
+      // Get the games.
+      $reserve['games'] = $this->playersService->getGames($node);
+
+      // Set the display date and actual date in the games array.
+      $reserve['display_date'] = date('l F j, Y', strtotime($date));
+      $reserve['date'] = $date;
+    }
+    else {
+      $reserve['display_date'] = date('l F j, Y', strtotime($date));
+    }
+
+    // Return custom template with variable.
+    return [
+      '#theme' => 'cbl_reserve',
+      '#reserve' => $reserve,
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+
+  }
+
+}
+
